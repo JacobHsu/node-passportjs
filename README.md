45aee6312c17ed9c13fe6a7261686cd19428999d
jacobhsu <jacob.hsu.tw@gmail.com>
2022-08-25 14:38:32 +0800

    fix: passportjs

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Simple, unobtrusive authentication for Node.js
 React Social Login with Passport.js | React oAuth w/ Google, Facebook, Github
 [https://www.youtube.com/watch?v=7K9kDrtc4S8](https://www.youtube.com/watch?v=7K9kDrtc4S8)  
 [Source Code](https://github.com/safak/youtube/tree/react-social-login)
+
+## Debug
+
+> TypeError: req.session.regenerate is not a function using Passport
+
+npm install passport@0.5
+
+[passport](https://www.npmjs.com/package/passport) 0.6.0 降版到 0.5.3
--- routes/auth.js
+++ routes/auth.js
@@ -1,36 +1,39 @@
-const router = require('express').Router();
-const passport = require('passport');
+const router = require("express").Router();
+const passport = require("passport");
 
-const CLIENT_URL = 'http://localhost:3000/';
+const CLIENT_URL = "http://localhost:3000/";
 
-router.get('/login/success', (req, res) => {
+router.get("/login/success", (req, res) => {
   if (req.user) {
     res.status(200).json({
       success: true,
-      message: 'successfull',
+      message: "successfull",
       user: req.user,
       //   cookies: req.cookies
     });
   }
 });
 
-router.get('/login/failed', (req, res) => {
+router.get("/login/failed", (req, res) => {
   res.status(401).json({
     success: false,
-    message: 'failure',
+    message: "failure",
   });
 });
 
-// https://www.passportjs.org/packages/passport-google-oauth20/
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect(CLIENT_URL);
+});
 
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 
 router.get(
-  '/google/callback',
-  passport.authenticate('google', {
+  "/google/callback",
+  passport.authenticate("google", {
     successRedirect: CLIENT_URL,
-    failureRedirect: '/login/failed',
+    failureRedirect: "/login/failed",
   })
 );
 
-module.exports = router;
+module.exports = router
