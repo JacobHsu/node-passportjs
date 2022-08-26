443cbbdb3f9cc1f50fbffadbe2bed2ad41c52302
jacobhsu <jacob.hsu.tw@gmail.com>
2022-08-26 10:43:34 +0800

    fix: This Serverless Function has crashed

--- routes/auth.js
+++ routes/auth.js
@@ -40,6 +40,6 @@ router.get('/logout', (req, res) => {
 
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 
-router.get('/google/callback', passport.authenticate('google', { successRedirect: CLIENT_URL, failureRedirect: '/login/failed' }));
+router.get('/google/callback', passport.authenticate('google', { successRedirect: CLIENT_URL, failureRedirect: '/login/failed',}));
 
 module.exports = router;
