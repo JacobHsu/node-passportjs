95d3a157f9c1f21aefb8c468206aab25c573330b
jacobhsu <jacob.hsu.tw@gmail.com>
2022-08-26 10:57:29 +0800

    nodemon start

--- routes/auth.js
+++ routes/auth.js
@@ -3,18 +3,6 @@ const passport = require('passport');
 
 const CLIENT_URL = 'http://localhost:3000/';
 
-router.get('/', async (req, res) => {
-  try {
-    res.json({
-      status: 200,
-      message: 'Get data has successfully',
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send('Server error');
-  }
-});
-
 router.get('/login/success', (req, res) => {
   if (req.user) {
     res.status(200).json({
